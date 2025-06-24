5cd952edac48abf49f31cc787c1cff2b8e86ca83
KieuAnh1603 <kieutuananh0316@gmail.com>
2025-06-24 10:42:33 +0700

    sua bai tap 24\6

--- BaiTapLuyenOOP/ThuVien/Maintest.java
+++ BaiTapLuyenOOP/ThuVien/Maintest.java
@@ -4,8 +4,11 @@ public class Maintest {
     public static void main(String[] args) {
         Book b1 = new Book("001", "lap trinh code", "A");
         Book b2 = new Book("002", "cau truc du lieu", "B");
+        Book b3 = new Book("003", "Giai thuat", "C");
+        Book b4 = new Book("004", "con tro", "D");
 
         Member m1 = new Member("M001", "Tuan Anh ");
+        Member m2 = new Member("M002", "Sushi ");
         m1.borrowBook(b2);
         m1.borrowBook(b1);
         m1.borrowBook(b2);
@@ -14,5 +17,12 @@ public class Maintest {
 
         m1.returnBook(b1);
         m1.showBorrowedBook();
+
+
+        m2.borrowBook(b4);
+        m2.borrowBook(b3);
+        m2.borrowBook(b2);
+        m2.borrowBook(b1);
+        m2.showBorrowedBook();
     }
 }
